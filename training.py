5ebd822131ae87a455dfc71a94d1680e967aebe8
Andrei3223 <a.gruzitski@gmail.com>
2024-03-21 12:34:24 +0300

    add checkpoint

--- training.py
+++ training.py
@@ -59,9 +59,11 @@ def training_epoch(model, optimizer, criterion, train_loader,
 
 @torch.no_grad()
 def validation_epoch(model, criterion, test_loader,
-                      tqdm_desc, device='cpu', threshold=0.05):
+                      tqdm_desc, epoch, device='cpu', threshold=0.05):
     test_loss = 0.0
     acc = 0
+    # output = np.empty((64, 100, 19))
+
     model.eval()
 
     for feat, labels in tqdm(test_loader, desc=tqdm_desc):
@@ -72,14 +74,21 @@ def validation_epoch(model, criterion, test_loader,
         res = model(feat, feat)  # src=tgt 
         loss = criterion(res, feat)
 
-        full_loss = torch.nn.MSELoss(reduce=False)(res, feat)
+        full_loss = torch.nn.MSELoss(reduction='none')(res, feat)
         acc  += evaluate_loss(full_loss, labels)
 
         test_loss += loss.item() * feat.shape[0]
+
+        # output = np.concatenate((output, res.detach().cpu().numpy()), axis=0)
         
 
     test_loss /= len(test_loader.dataset)
     acc /= len(test_loader.dataset)
+
+    # if (epoch - 1) % 5 == 0:
+    #     plot_output(model, test_loader, epoch, device=device, val_output=output)
+    #     if epoch != 0:
+    #         torch.save(model.state_dict(), f'.\checkpoints\AE_{epoch - 1}e.pt')
 
     return test_loss, acc
 
@@ -90,7 +99,7 @@ def train(model, optimizer, scheduler, criterion,
     train_losses, train_accuracies = [], []
     test_losses, test_accuracies = [], []
 
-    for epoch in range(1, num_epochs + 1):
+    for epoch in range(1, num_epochs + 1):             # !!!!!!!
         train_loss, train_accuracy = training_epoch(
             model, optimizer, criterion, train_loader,
             tqdm_desc=f'Training {epoch}/{num_epochs}',
@@ -100,6 +109,7 @@ def train(model, optimizer, scheduler, criterion,
         test_loss, test_accuracy = validation_epoch(
             model, criterion, test_loader,
             tqdm_desc=f'Validating {epoch}/{num_epochs}',
+            epoch=epoch,
             device=device,
             threshold=threshold,
         )
@@ -112,14 +122,14 @@ def train(model, optimizer, scheduler, criterion,
         test_losses += [test_loss]
         test_accuracies += [test_accuracy]
         # test_f1_scores += [test_f1]
-        plot_losses(train_losses, test_losses, train_accuracies, test_accuracies)
+        # plot_losses(train_losses, test_losses, train_accuracies, test_accuracies)
         wandb.log({'train_loss': train_loss, 'val_loss': train_loss,
                     "train_acc": train_accuracy, "val_acc": test_accuracy})
+        
 
-        if epoch % 10 == 0:
-            plot_output(model, test_loader, device)
-            if epoch != 0:
-                torch.save(model.state_dict(), f'.\checkpoints\AE_{epoch}e.pt')
+        plot_output(model, test_loader, epoch, device=device)
+        if epoch != 0 and epoch % 10 == 0:
+            torch.save(model.state_dict(), f'.\checkpoints\AE_{epoch - 1}e.pt')
 
     return train_losses, test_losses, train_accuracies, test_accuracies
 
@@ -137,9 +147,9 @@ def evaluate_loss(loss, labels, thresh=0.05):
 
 
 @torch.no_grad()
-def get_series(model, test_loader,  device='cpu', threshold=0.05):
+def get_series(model, test_loader,  device='cpu', threshold=0.1):
     res_result = np.empty((64, 100, 19))
-    for feat, labels in tqdm(test_loader):
+    for feat, labels in test_loader:
         feat = feat.to(device)
         labels = torch.Tensor(labels)
         labels = labels.to(device)  # labels: batch_size
@@ -155,18 +165,32 @@ def array_from_loader(loader):
     return torch.cat(data_list).numpy()
 
 @torch.no_grad()
-def plot_output(model, loader, device='cpu'):
-    val_output = get_series(model, loader,  device=device)
-    val_input = array_from_loader(loader)
-    fig, axs = plt.subplots(2 * val_output.shape[1], 1, figsize =(10, 70))
+def plot_output(model, loader, epoch, device='cpu', val_output=None):
+    clear_output()
+    if val_output is None:
+        val_output = get_series(model, loader,  device=device).reshape(-1, 19)
+    else:
+        val_output = val_output.reshape(-1, 19)
+
+    val_input = array_from_loader(loader).reshape(-1, 19)
+
+    num_plots = min(7, val_output.shape[1])
+    fig, axs = plt.subplots(2 * num_plots, 1, figsize =(10, 30))
     fig.tight_layout()
-    for i in range(val_output.shape[1]):
+    # wandb_dict = {}
+    for i in range(num_plots):
+        axs[2*i+1].set_ylim([-20, 20])
         axs[2*i].plot(val_input[:, i])
-        axs[2*i+1].plot(np.where(val_output[:, i] > 10 or val_output[:, i] < -10, 3, val_output[:, i]))
+        axs[2*i+1].plot(val_output[:, i])
 
         axs[2*i+1].set_title(f"AE output [{i}]", fontsize=7)
         axs[2*i].set_title(f"AE input [{i}]", fontsize=7)
 
-    
+        # wandb_dict[f'input[{i}]'] = val_input[:, i]
+        # wandb_dict[f'output[{i}]'] = np.where(((val_output[:, i] > 10) | (val_output[:, i] < -10)), 3, val_output[:, i])
+
+    # wandb.log(wandb_dict)
+    # fig.savefig(f'{epoch}e_output')
+    plt.show()
 
      
